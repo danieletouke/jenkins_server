e37ec7f3a8b09c688e76d3d63d8e9d04d4e6de8d
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-01-22 08:14:51 -0500

    fixes to pipeline_script

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -5,19 +5,19 @@ pipeline {
         stage('Cloning') {
             steps {
                 echo 'Cloning...'
-                sh 'source 1clone_and_pull_remote_repo.sh'   
+                sh 'source /var/lib/jenkins/1clone_and_pull_remote_repo.sh'   
             }
         }
         stage('Checking') {
             steps {
                 echo 'Checking for yaml file...'
-                sh 'source 2check_for_yaml_file_get_aws_region.sh'
+                sh 'source /var/lib/jenkins/2check_for_yaml_file_get_aws_region.sh'
             }
         }
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
-                sh 'source 3terraform_apply.sh'
+                sh 'source /var/lib/jenkins/3terraform_apply.sh'
             }
         }  
     }
