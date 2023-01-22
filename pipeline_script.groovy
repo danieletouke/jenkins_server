30b46937d6afefa60d56924691fb5ac29f48af85
danieletouke <108566435+danieletouke@users.noreply.github.com>
2023-01-22 06:45:32 -0500

    update the pipeline groovy file

--- pipeline_script.groovy
+++ pipeline_script.groovy
@@ -5,40 +5,20 @@ pipeline {
         stage('Cloning') {
             steps {
                 echo 'Cloning...'
-
-               
-               
+                sh 'source 1clone_and_pull_remote_repo.sh'   
             }
         }
-        stage('Build') {
+        stage('Checking') {
             steps {
-                echo 'BUilding...'
-
-                sh 'docker build -t Dockerfile'
-
-                
-
+                echo 'Checking for yaml file...'
+                sh 'source 2check_for_yaml_file_get_aws_region.sh'
             }
         }
         stage('Deploy') {
             steps {
                 echo 'Deploying....'
+                sh 'source 3terraform_apply.sh'
             }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
+        }  
     }
 }
